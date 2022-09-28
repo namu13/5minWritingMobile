8ec14499453ba2f111eb9f4b92cb8e341885c97a
namu <namun1303@gmail.com>
2022-09-29 01:09:25 +0900

    realm database setup

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,28 +8,61 @@ import AppLoading from "expo-app-loading";
 import Home from "./screen/Home";
 import Editor from "./screen/Editor";
 import { useState } from "react";
+import { useEffect } from "react";
+import { DBContext } from "./context";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [loaded] = useFonts({
+  const [databaseLoaded, setDatabaseLoaded] = useState(false);
+  const [realm, setRealm] = useState(null);
+
+  const DocumentSchema = {
+    name: "Document",
+    properties: {
+      _id: "int",
+      title: "string",
+      mainText: "string",
+      timeStamp: "string",
+    },
+    primaryKey: "_id",
+  };
+
+  const realmOpen = async () => {
+    const connection = await Realm.open({
+      path: "fiveMinWritingDB",
+      schema: [DocumentSchema],
+    });
+    setRealm(connection);
+    setDatabaseLoaded(true);
+  };
+
+  useEffect(() => {
+    realmOpen();
+  }, []);
+
+  const [fontLoaded] = useFonts({
     Snow: require("./assets/Fonts/SF_HambakSnow.otf"),
     Mapo: require("./assets/Fonts/MapoGoldenPier.otf"),
   });
-  if (!loaded) {
+
+  if (!fontLoaded || !databaseLoaded) {
     return <AppLoading />;
   }
+
   return (
-    <NavigationContainer>
-      <StatusBar style="inverted" />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Editor" component={Editor} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <DBContext.Provider value={realm}>
+      <NavigationContainer>
+        <StatusBar style="inverted" />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Editor" component={Editor} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </DBContext.Provider>
   );
 }
